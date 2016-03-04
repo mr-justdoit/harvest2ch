36a19b1863b5643b6cdc879a3e6353070439b52e
mr-justdoit <jasonss772@gmail.com>
2016-03-04 14:35:17 +0900

    update

--- src/harvest2ch.py
+++ src/harvest2ch.py
@@ -40,30 +40,35 @@ def format_dt(metatexts):
 
 def feature_extraction(texts, metatexts):
     metatexts = format_dt(metatexts)
+    vector = []
     for i in range(0, len(texts)):
         V1 = len(texts[i])
-        V2 = len(re.findall('、', texts[i])) / V1
-        V3 = len(re.findall(r'。', texts[i])) / V1
-        V4 = len(re.findall('\u3000', texts[i])) / V1
-        V5 = len(re.findall('\n', texts[i])) / V1
-        V6 = len(re.findall(' ', texts[i])) / V1
-        V7 = len(re.findall(
+        vector.append(V1)
+        vector.append(len(re.findall('、', texts[i])) / V1)
+        vector.append(len(re.findall(r'。', texts[i])) / V1)
+        vector.append(len(re.findall('\u3000', texts[i])) / V1)
+        vector.append(len(re.findall('\n', texts[i])) / V1)
+        vector.append(len(re.findall(' ', texts[i])) / V1)
+        vector.append(len(re.findall(
             r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?',
-            texts[i]))
+            texts[i])))
         texts[i] = re.sub(
             r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?',
             "", texts[i])
-        V8 = len(re.findall('&gt;&gt;', texts[i]))
-        V9 = len(re.findall('？', texts[i])) / V1
-        V10 = len(re.findall('！', texts[i])) / V1
-        V11 = len(re.findall(r'[wｗ]', texts[i])) / V1
-        V12 = int("green" in metatexts[i])
-        V13 = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', metatexts[i]).pop()
-        V13 += ' '
-        V13 += re.findall(r'[0-9]{2}:[0-9]{2}:[0-9]{2}', metatexts[i]).pop()
-        V13 += ' JST'
-        print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" %
-              (V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13))
+        vector.append(len(re.findall('&gt;&gt;', texts[i])))
+        vector.append(len(re.findall('？', texts[i])) / V1)
+        vector.append(len(re.findall('！', texts[i])) / V1)
+        vector.append(len(re.findall(r'[wｗ]', texts[i])) / V1)
+        vector.append(int("green" in metatexts[i]))
+        timedata = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}',
+                              metatexts[i]).pop()
+        timedata += ' '
+        timedata += re.findall(r'[0-9]{2}:[0-9]{2}:[0-9]{2}',
+                               metatexts[i]).pop()
+        timedata += ' JST'
+        vector.append(timedata)
+        print(','.join([str(i) for i in vector]))
+        vector = []
 
 
 def main():
